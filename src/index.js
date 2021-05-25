99aaf618328042ed7597a52aa689d51a2f6ebd66
jhesus <jhesus.legarda@imaginecx.co>
2021-05-25 08:49:27 -0500

    updated project

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const port = 5000;
 const app = express();
 app.get('/', (req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write("<html><head><meta charset='utf-8' /></head><body><h1>HOLA</h1></body></html>");
-    res.end();
+    /* res.writeHead(200, { 'Content-Type': 'text/html' });
+     res.write("<html><head><meta charset='utf-8' /></head><body><h1>HOLA</h1></body></html>");
+     res.end();*/
+    res.status(200).send("<html><head><meta charset='utf-8' /></head><body><h1>HOLA</h1></body></html>");
 });
 app.listen(port, () => { console.log('PORT:' + port); });
